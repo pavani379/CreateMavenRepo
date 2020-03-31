9132ddd28d79c4df704445afcd065d6fff6f6a59
=chinni <=pavanivatrapu379@gmail.com>
2020-03-31 12:46:30 +0530

    pushing the project to the github repo

---
+++ CreateMavenRepo/src/main/java/com/accenture/spark/release/CreateMavenRepo/App.java
@@ -0,0 +1,13 @@
+package com.accenture.spark.release.CreateMavenRepo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "pushing the project to github repo" );
+    }
+}
